8896a58ba2950952004623fead36be5fcdc49c0c
lixuelin <370281730@qq.com>
2016-10-25 14:10:15 +0800

    加入百度统计

---
+++ js/count.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "//hm.baidu.com/hm.js?65577e6118ebd508bb34a6075cce2283";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
